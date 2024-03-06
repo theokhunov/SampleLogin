48232c2fd1fd8111330c4ce2cd8efde1d3b996fa
theok <theokhunov@gmail.com>
2024-03-06 11:09:27 +0500

    SampleLogin App

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var btn_new_activity = findViewById(R.id.createAcBtn) as TextView
+
+        btn_new_activity.setOnClickListener {
+            var intent = Intent(this,Create_Layout::class.java)
+            startActivity(intent)
+        }
+    }
+}
